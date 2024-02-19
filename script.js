60cb5c05be717b078b657373b585180a91ff6bf8
Pedro Mendoza <pedro.mendoza.b14@gmail.com>
2024-02-19 13:13:45 -0600

    change: Added button and index.html

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Bienvenido al juego pokemon")
+
+//realizar una funcion con alert
+
+function enviarSaludo(){
+    alert('Hola rata inmunda');
+}
+
+function saludarPersona(nombre){
+      nombre = prompt("Dime tu nombre");
+      alert(`${nombre} pica papas`);
+    }
